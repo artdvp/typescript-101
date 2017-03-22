638ba5747b69ab50af784828f1529cb2a5c48a08
bonbonpa <sphinspf@gmail.com>
2017-03-22 13:15:23 +0000

    Array string

--- app.js
+++ app.js
@@ -12,3 +12,9 @@ document.getElementById("id1").innerHTML = names;
 document.write("Count is Type: " + typeof (count) + "<br>");
 document.write("names is Type: " + typeof (names) + "<br>");
 document.write("strstatus is Type: " + typeof (strstatus) + "<br>");
+//charp 3 Array
+var employee = ["A", "B", "C"];
+employee.push("D");
+employee.push("GG");
+employee.push("500");
+console.log(employee.toString());
