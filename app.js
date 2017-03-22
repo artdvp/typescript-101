85f07bdaa3364438d19cee2d1ffd1e9644057727
bonbonpa <sphinspf@gmail.com>
2017-03-22 13:08:09 +0000

    chapter2 : Variable

---
+++ app.js
@@ -0,0 +1,14 @@
+//Variable
+var names = "Hello TS";
+var count = 5000;
+var strstatus = true;
+var aName = true; //use any type
+console.log("Hello World");
+console.log(names);
+console.log(count);
+console.log(strstatus);
+var aa = "aaa"; //
+document.getElementById("id1").innerHTML = names;
+document.write("Count is Type: " + typeof (count) + "<br>");
+document.write("names is Type: " + typeof (names) + "<br>");
+document.write("strstatus is Type: " + typeof (strstatus) + "<br>");
---
+++ app.ts
@@ -0,0 +1,20 @@
+
+
+//Variable
+var names:string = "Hello TS";
+var count:number = 5000;
+var strstatus:boolean = true;
+
+let aName:any = true ; //use any type
+
+console.log("Hello World");
+console.log(names);
+console.log(count);
+console.log(strstatus);
+
+let aa = "aaa"; //
+
+document.getElementById("id1").innerHTML= names ; 
+document.write("Count is Type: " + typeof(count) + "<br>");
+document.write("names is Type: " + typeof(names) + "<br>");
+document.write("strstatus is Type: " + typeof(strstatus) + "<br>");
